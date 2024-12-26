68bfc133fc4bd9217f423944718dce334668ae22
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 02:37:20 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.pinterestdl('https://pin.it/mRzIJmrED');
+    const asupanfilmResult = await Qasim.bacaresep('nature');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.gempa();
+    const twitterResult = await Qasim.searchgore('hot');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.cariresep('https://resepkoki.id/resep/resep-ayam-woku/');
+    const mediaResult = await Qasim.randomgore();
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
