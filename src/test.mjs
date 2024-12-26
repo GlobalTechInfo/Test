1b18244ee5f985e64b940a2c6dfdb4ae17619e5e
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 04:24:24 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,10 +3,10 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.palingmurah('netflix');
+    const asupanfilmResult = await Qasim.pinterest('netflix');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.wallpaper('nature');
+    const twitterResult = await Qasim.happymod('telegram');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
