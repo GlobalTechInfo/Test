aee5ef6c475e588f7877e472fcd14fb37934f57e
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 00:34:46 +0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
-// index.mjs
+// index.mjs (ES Module entry)
+
 import { tiktokDl, xdown, MediaUmma, wikimedia, styletext, ringtone, devianart, konachan, wallpapercave, wallpapercraft, wallpaperhd } from './apiqasim.js';
 
 const Qasim = { tiktokDl, xdown, MediaUmma, wikimedia, styletext, ringtone, devianart, konachan, wallpapercave, wallpapercraft, wallpaperhd };
