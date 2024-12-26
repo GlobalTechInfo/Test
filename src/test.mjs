6ae19f3cde79f17390f244cca16b076f3c05d0ae
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 03:38:26 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.konachan('neko');
+    const asupanfilmResult = await Qasim.wallpaperhd('nature');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.wallpapercraft('nature');
+    const twitterResult = await Qasim.styletext('Qasim');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.wattpad('telegram');
+    const mediaResult = await Qasim.ringtone('nokia');
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
