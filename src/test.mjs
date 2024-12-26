b5f1d04d31bed372e2a76924671143ddf773efdb
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 01:57:46 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.job('manager');
+    const asupanfilmResult = await Qasim.anoboy('manager');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.happymoddl('https://happymod.com/race-max-pro-car-racing-mod/game.revani.racemaxpro');
+    const twitterResult = await Qasim.anoboydl('doctor');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.zerochan('ignite');
+    const mediaResult = await Qasim.film('ignite');
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
