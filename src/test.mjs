aad73377011f25014e7ef42e76278cccdd0ed1d6
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 02:33:39 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.ssweb('https://github.com/GlobalTechInfo/Test/blob/main/src/test.mjs');
+    const asupanfilmResult = await Qasim.pinterestdl('https://pin.it/mRzIJmrED');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.facebook('https://www.facebook.com/share/r/14mBGGz8U7');
+    const twitterResult = await Qasim.gempa();
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.film('Red');
+    const mediaResult = await Qasim.cariresep('https://resepkoki.id/resep/resep-ayam-woku/');
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
