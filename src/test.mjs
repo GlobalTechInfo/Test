d5f567a93bb32fce831c2feca2e405d0d7cfebe6
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 03:20:58 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.android1('telegram');
+    const asupanfilmResult = await Qasim.artinama('Abhimata');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.mangatoon('ring');
+    const twitterResult = await Qasim.drakor('nature');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.happymod('telegram');
+    const mediaResult = await Qasim.wattpad('telegram');
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
