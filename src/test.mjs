7903e3d54b638aa9aa4a220af6ccaeb51fded83a
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 01:46:02 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const asupanfilmResult = await Qasim.trendtwit('Pakistan');
+    const asupanfilmResult = await Qasim.chara('Naruto UZUMAKI');
     console.log('TikTok Video:', asupanfilmResult);
 
-    const twitterResult = await Qasim.pinterest('ignite');
+    const twitterResult = await Qasim.happymoddl('https://happymod.com/race-max-pro-car-racing-mod/game.revani.racemaxpro');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.rexdldown('https://rexdl.com/android/rope-hero-vice-town');
+    const mediaResult = await Qasim.zerochan('ignite');
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
