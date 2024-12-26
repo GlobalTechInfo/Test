01a053cba850d41ef4dfc7a4952e86c6f2c4645e
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-26 23:44:27 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 const { tiktokDl, xdown, MediaUmma, wikimedia, styletext, ringtone, devianart, konachan, wallpapercave, wallpapercraft, wallpaperhd } = require('./apiqasim');
 
+const Qasim = { tiktokDl, xdown, MediaUmma, wikimedia, styletext, ringtone, devianart, konachan, wallpapercave, wallpapercraft, wallpaperhd };
+
+module.exports = {
+  Qasim
+};
