25e939cda09475b33532e379e717b167b3b3ba8c
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 01:17:43 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,7 +3,7 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const rexdlResult = await Qasim.rexdl('Telegram');
+    const rexdlResult = await Qasim.asupanfilm('Red');
     console.log('TikTok Video:', tiktokResult);
 
     const twitterResult = await Qasim.merdekanews();
