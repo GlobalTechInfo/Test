5a9fb47180624f6d37650c48e4ba36afe4177272
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 03:58:31 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-// index.js (CommonJS entry)
+const { 
+  merdekanews, stickersearch, trendtwit, zerochan, anime, job, anoboys, film, 
+  webtoons, soundcloud, facebook, ssweb, pinterestdl, gempa, cariresep, bacaresep, 
+  textmakervid, apkmirror, zippydl, android1, happymod, nickff, nickml, corona, 
+  mangatoon, palingmurah, mediafire, artinama, wattpad, sfilesearch, carigc, wikisearch, 
+  devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
+  wikimedia, tiktokDl, xdown 
+} = require('./apiqasim');
 
-const { merdekanews, stickersearch, trendtwit, zerochan, anime, job, anoboys, film, webtoons, soundcloud, facebook, ssweb, pinterestdl, gempa, cariresep, bacaresep, textmakervid, apkmirror, zippydl, android1, happymod, nickff, nickml, corona, mangatoon, palingmurah, mediafire, artinama, wattpad, sfilesearch, carigc, wikisearch, devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, wikimedia, tiktokDl, xdown } = require('./apiqasim');
-
-const Qasim = { merdekanews, stickersearch, trendtwit, zerochan, anime, job, anoboys, film, webtoons, soundcloud, facebook, ssweb, pinterestdl, gempa, cariresep, bacaresep, textmakervid, apkmirror, zippydl, android1, happymod, nickff, nickml, corona, mangatoon, palingmurah, mediafire, artinama, wattpad, sfilesearch, carigc, wikisearch, devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, wikimedia, tiktokDl, xdown };
+const Qasim = {
+  merdekanews, stickersearch, trendtwit, zerochan, anime, job, anoboys, film, 
+  webtoons, soundcloud, facebook, ssweb, pinterestdl, gempa, cariresep, bacaresep, 
+  textmakervid, apkmirror, zippydl, android1, happymod, nickff, nickml, corona, 
+  mangatoon, palingmurah, mediafire, artinama, wattpad, sfilesearch, carigc, wikisearch, 
+  devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
+  wikimedia, tiktokDl, xdown
+};
 
 module.exports = Qasim;
