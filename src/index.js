7107f0d82861040b6e4a24dd2d04a50b97f97722
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 14:16:13 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const { 
-  merdekanews, stickersearch, trendtwit, zerochan, job, anoboys, film, bitly,
+  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
   webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
   textmakervid, apkmirror, weather, android1, happymod, nickff, nickml, corona, 
-  mangatoon, palingmurah, quotesanime, artinama, wattpad, playstore, carigc, wikisearch, 
-  devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
+  konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown 
 } = require('./apiqasim');
 
 const Qasim = {
-  merdekanews, stickersearch, trendtwit, zerochan, job, anoboys, film, bitly,
+  merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
   webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
   textmakervid, apkmirror, weather, android1, happymod, nickff, nickml, corona, 
-  mangatoon, palingmurah, quotesanime, artinama, wattpad, playstore, carigc, wikisearch, 
-  devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
+  mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
+  konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown
 };
 
