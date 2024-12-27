c01678dd2efd5831932247a0ed6c32e4a18418d0
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 12:19:06 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const { 
-  merdekanews, stickersearch, trendtwit, zerochan, anime, job, anoboys, film, 
-  webtoons, soundcloud, facebook, ssweb, pinterestdl, gempa, cariresep, bacaresep, 
-  textmakervid, apkmirror, zippydl, android1, happymod, nickff, nickml, corona, 
-  mangatoon, palingmurah, mediafire, artinama, wattpad, sfilesearch, carigc, wikisearch, 
+  merdekanews, stickersearch, trendtwit, zerochan, job, anoboys, film, bitly,
+  webtoons, soundcloud, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
+  textmakervid, apkmirror, weather, android1, happymod, nickff, nickml, corona, 
+  mangatoon, palingmurah, quotesanime, artinama, wattpad, playstore, carigc, wikisearch, 
   devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown 
 } = require('./apiqasim');
 
 const Qasim = {
-  merdekanews, stickersearch, trendtwit, zerochan, anime, job, anoboys, film, 
-  webtoons, soundcloud, facebook, ssweb, pinterestdl, gempa, cariresep, bacaresep, 
-  textmakervid, apkmirror, zippydl, android1, happymod, nickff, nickml, corona, 
-  mangatoon, palingmurah, mediafire, artinama, wattpad, sfilesearch, carigc, wikisearch, 
+  merdekanews, stickersearch, trendtwit, zerochan, job, anoboys, film, bitly,
+  webtoons, soundcloud, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
+  textmakervid, apkmirror, weather, android1, happymod, nickff, nickml, corona, 
+  mangatoon, palingmurah, quotesanime, artinama, wattpad, playstore, carigc, wikisearch, 
   devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown
 };
