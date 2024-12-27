092af7fd30a724db592ade5cbc52a6a4099d0436
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 20:27:30 +0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -5,7 +5,7 @@ import {
   mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown 
-} from './api-qasimm.js';
+} from './api-qasimm.mjs';
 
 const Qasim = {
   merdekanews, stickersearch, trendtwit, zerochan, bitly,
