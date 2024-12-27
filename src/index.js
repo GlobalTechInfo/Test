93d6f2d9241caddaf7e55e4a32f9fd4ea46dc7a4
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 19:40:35 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { 
   merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
   webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
-  textmakervid, apkmirror, weather, android, happymod, nickff, nickml, corona, 
+  textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, 
   mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown 
@@ -10,7 +10,7 @@ const {
 const Qasim = {
   merdekanews, stickersearch, trendtwit, zerochan, anoboys, film, bitly,
   webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
-  textmakervid, apkmirror, weather, android, happymod, nickff, nickml, corona, 
+  textmakervid, apkmirror, weather, apksearch, happymod, nickff, nickml, corona, 
   mangatoon, quotesanime, artinama, wattpad, playstore, wikisearch, 
   konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
   wikimedia, tiktokDl, xdown
