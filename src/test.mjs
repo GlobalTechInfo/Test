57ab09ee8b64f91d73ff8e8865c13451cf2482b7
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 13:12:43 +0500

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -3,14 +3,14 @@ import Qasim from './index.mjs';
 
 async function test() {
   try {
-    const WeatherResult = await Qasim.happymod('telegram');
+    const WeatherResult = await Qasim.facebook('https://www.facebook.com/share/r/1D5NfahNB8/');
     console.log('Weather:', WeatherResult);
 
-    const QuotesResult = await Qasim.android1('whatsapp');
+    const QuotesResult = await Qasim.wikisearch('whatsapp');
     console.log('Anime Quotes:', QuotesResult);
 
     // Test other functions similarly
-    const PlaystoreResult = await Qasim.pinterest('ignite');
+    const PlaystoreResult = await Qasim.stickersearch('ignite');
     console.log('Play Store:', PlaystoreResult);
 
   } catch (error) {
