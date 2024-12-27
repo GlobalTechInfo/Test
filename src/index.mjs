018e3cbef37625642e40f9e7a781399bfddf0c82
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 13:08:22 +0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import { 
   merdekanews, stickersearch, trendtwit, zerochan, job, anoboys, film, bitly,
-  webtoons, soundcloud, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
+  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl, 
   textmakervid, apkmirror, weather, android1, happymod, nickff, nickml, corona, 
   mangatoon, palingmurah, quotesanime, artinama, wattpad, playstore, carigc, wikisearch, 
   devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
@@ -9,7 +9,7 @@ import {
 
 const Qasim = {
   merdekanews, stickersearch, trendtwit, zerochan, job, anoboys, film, bitly,
-  webtoons, soundcloud, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
+  webtoons, facebook, ssweb, pinterest, gempa, cariresep, tinyurl,
   textmakervid, apkmirror, weather, android1, happymod, nickff, nickml, corona, 
   mangatoon, palingmurah, quotesanime, artinama, wattpad, playstore, carigc, wikisearch, 
   devianart, konachan, wallpapercraft, wallpaper, styletext, ringtone, mediaumma, 
