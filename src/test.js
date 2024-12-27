9c2280caeecc30105c5bc6f41523321673b17c09
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-27 13:10:33 +0500

    Update test.js

--- src/test.js
+++ src/test.js
@@ -2,14 +2,14 @@ const Qasim = require('./index');
 
 async function test() {
   try {
-    const tiktokResult = await Qasim.soundcloud('https://on.soundcloud.com/cqEntUzm36EjbEwW7');
+    const tiktokResult = await Qasim.wallpapercraft('sky');
     console.log('TikTok Video:', tiktokResult);
 
     const twitterResult = await Qasim.wallpaper('nature');
     console.log('X Video:', twitterResult);
 
     // Test other functions similarly
-    const mediaResult = await Qasim.apkmirror('telegram');
+    const mediaResult = await Qasim.ssweb('https://github.com/GlobalTechInfo/Test');
     console.log('Media Umma:', mediaResult);
 
   } catch (error) {
